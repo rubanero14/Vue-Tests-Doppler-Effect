b8b34d47a5d7da695835e247184bd2bea7c63b74
rubanero14 <85555894+rubanero14@users.noreply.github.com>
2022-03-31 13:25:46 +0800

    Add files via upload

--- App.vue
+++ App.vue
@@ -1,31 +1,39 @@
 <template>
+  <header>
+      <h1 class="mb-0">Doppler Effect Demo</h1>
+  </header>
   <div class="container">
     <div class="circle" :style="{ 'background': this.finalColor, 'transform':   this.transform }">
       <p v-if="inputValue < -10" class="text-light">Blue Star</p>
       <p v-else-if="inputValue > 10" class="text-light">Red Star</p>
       <p v-else class="text-secondary">Yellow Star</p>
     </div>
-    <div class="top-left">
-      <div class="d-flex align-items-center mb-5">
-        <input v-model="inputValue" @change="changeColors();" @input="changeColors();" class="w-100" id="inputRange" type="range" :min="min" :max="max" step="5">
-        <label class="ms-2" for="inputValue">{{ inputValue }}</label>
-      </div>
-      <div class="bottom-left">
-        <label class="mb-2">Enter Velocity (km/s):</label>
-        <input v-model="velocity" @change="velocityControl" class="form-control" placeholder="Enter velocity.." type="text">
-      </div>
-      <div>
-        <a class="btn btn-secondary top-right" target="_blank" href="https://github.com/rubanero14/Vue-Tests-Doppler-Effect">Source Code</a>
+  </div>
+  <footer class="my-3">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-md-6 col-lg-4">
+          <input v-model="inputValue" @change="changeColors();" @input="changeColors();" class="w-100 mb-3 mb-md-0" id="inputRange" type="range" :min="min" :max="max" step="5">
+        </div>
+        <div class="col-12 col-md-6 col-lg-4">
+          <div class="d-flex align-items-center">
+              <label class="mb-3 mb-md-0 me-2">Enter Velocity (km/s):</label>
+              <input v-model="velocity" @change="velocityControl" class="form-control w-auto mb-3 mb-md-0" type="number">
+          </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4">
+          <a class="btn btn-secondary w-100" target="_blank" href="https://github.com/rubanero14/Vue-Tests-Doppler-Effect">Source Code</a>
+        </div>
       </div>
     </div>
-  </div>
+  </footer>
 </template>
 
 <script>
 export default {
   data(){
     return {
-      inputValue: 0,
+      inputValue:'',
       velocity: 0,
       min: -100,
       max: 100,
@@ -44,51 +52,35 @@ export default {
       } else if(this.velocity > this.max){
         this.max = 100;
         this.velocity = this.max;
+      } else if(this.velocity == ''){
+        this.velocity = 0;
       } else {
-        this.inputValue = parseInt(this.velocity);
+        this.inputValue = this.velocity;
         console.log(typeof this.inputValue, this.inputValue);
       }
     },
   },
   watch: {
     inputValue: function changeColors(){
+      this.velocity = parseInt(this.inputValue);
       if (this.inputValue < 0){
         this.red = 255 + (this.inputValue * 2.55);
         this.blue = -(this.inputValue * 2.55);
         this.green = 255 + (this.inputValue * 2.55);
         this.finalColor = 'rgb('+ this.red + ',' + this.green + ',' + this.blue + ')';
-        this.transform = 'scale('+(2.5 - this.inputValue*0.015)+')';
-        console.log('blue activated');
-        console.log('Final color '+this.finalColor);
-        console.log('Green '+this.green);
-        console.log('Red '+this.red);
-        console.log('Blue '+this.blue);
-        console.log('Transform '+this.transform);
+        this.transform = 'scale('+(2.3 - this.inputValue*0.015)+')';
       } else if (this.inputValue > 0){
         this.red = 255;
         this.green = 255 - (this.inputValue * 2.55);
         this.blue = 0;
         this.finalColor = 'rgb('+ this.red + ',' + this.green + ',' + this.blue + ')';
         this.transform = 'scale('+(2.5 + this.inputValue*(-0.015))+')';
-        console.log('red activated');
-        console.log('Final color '+this.finalColor);
-        console.log('Green '+this.green);
-        console.log('Red '+this.red);
-        console.log('Blue '+this.blue);
-        console.log('Transform '+this.transform);
-        console.log('inputvalue '+this.inputValue);
       } else {
         this.green = 255 - (this.inputValue * 2.55);
         this.red = 255;
         this.blue = 0;
         this.finalColor = 'rgb('+ this.red + ',' + this.green + ',' + this.blue + ')';
         this.transform = 'scale('+(2.5 + this.inputValue*0.015)+')';
-        console.log('yellow activated');
-        console.log('Final color '+this.finalColor);
-        console.log('Green '+this.green);
-        console.log('Red '+this.red);
-        console.log('Blue '+this.blue);
-        console.log('Transform '+this.transform);
       }
     },
   },
@@ -102,15 +94,25 @@ body {
   background: #ccc;
 }
 
+header {
+  margin: 30px auto;
+  text-align: center;
+}
+
+footer {
+  margin: 30px auto;
+  text-align: center;
+}
+
 .top-left {
   position: fixed;
   top: 20px;
   left: 20px;
 }
 
-.top-right {
+.bottom-right {
   position: fixed;
-  top: 20px;
+  bottom: 20px;
   right: 20px;
 }
 
@@ -139,7 +141,7 @@ p {
   width: 150px;
   border: 1px solid #ccc;
   border-radius: 100%;
-  margin: 40vh auto;
+  margin: 30vh auto;
   transition: all 0.3s ease-in-out;
   background: rgb(255,255,0);
   transform: scale(2.5);
@@ -149,6 +151,7 @@ p {
   .circle {
     height: 100px;
     width: 100px;
+    margin: 30vh auto;
   }
   
   p {
